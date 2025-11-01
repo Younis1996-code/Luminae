82cac151b82bb946fcd36cf5c648596982c7617f
hasanWhatfa <wtfyh05@gmail.com>
2025-11-01 16:22:44 +0300

    firebase config and initial db schema

---
+++ src/data/firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSQkwa3AyIPL6qo2MwG6_medVG9d_0vvY",
+  authDomain: "luminae-ca224.firebaseapp.com",
+  projectId: "luminae-ca224",
+  storageBucket: "luminae-ca224.firebasestorage.app",
+  messagingSenderId: "1048878162907",
+  appId: "1:1048878162907:web:35f6d85f453c4c91827dfe"
+};
+
+export const app = initializeApp(firebaseConfig);
+
