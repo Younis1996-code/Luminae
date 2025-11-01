caa03b8cb8525da23b96ef5f9b7377893d603a17
hasanWhatfa <wtfyh05@gmail.com>
2025-11-01 16:51:26 +0300

    fix api problem

--- src/data/firebase/firebaseConfig.js
+++ src/data/firebase/firebaseConfig.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
+require('dotenv').config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSQkwa3AyIPL6qo2MwG6_medVG9d_0vvY",
-  authDomain: "luminae-ca224.firebaseapp.com",
-  projectId: "luminae-ca224",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
   storageBucket: "luminae-ca224.firebasestorage.app",
   messagingSenderId: "1048878162907",
-  appId: "1:1048878162907:web:35f6d85f453c4c91827dfe"
+  appId: process.env.APP_ID
 };
 
 export const app = initializeApp(firebaseConfig);
